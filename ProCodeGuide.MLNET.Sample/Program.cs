7bdb6a4bf0938a5d532765883acdde3300878c4d
Procoder <support@procodeguide.com>
2021-01-31 20:00:43 +0530

    Add project files.

---
+++ ProCodeGuide.MLNET.Sample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ProCodeGuide_MLNET_SampleML.Model;
+
+namespace ProCodeGuide.MLNET.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add input data
+            var input = new ModelInput();
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+
+        }
+    }
+}
