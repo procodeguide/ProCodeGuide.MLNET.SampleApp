e65c63eb3a08abe253bc0e51675c6f28c101bfa3
Procoder <support@procodeguide.com>
2021-01-31 21:17:52 +0530

    Change to program.cs

--- ProCodeGuide.MLNET.Sample/Program.cs
+++ ProCodeGuide.MLNET.Sample/Program.cs
@@ -8,11 +8,16 @@ namespace ProCodeGuide.MLNET.Sample
         static void Main(string[] args)
         {
             // Add input data
-            var input = new ModelInput();
+            var input = new ModelInput()
+            {
+                Col0 = "This restaurant was wonderful."
+            };
 
             // Load model and predict output of sample data
             ModelOutput result = ConsumeModel.Predict(input);
-
+            // If Prediction is 1, sentiment is "Positive"; otherwise, sentiment is "Negative"
+            string sentiment = result.Prediction == "1" ? "Positive" : "Negative";
+            Console.WriteLine($"Text: {input.Col0}\nSentiment: {sentiment}");
         }
     }
 }
